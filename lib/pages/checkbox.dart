91d3e2580b8b319348d5085f9dc1c81a2b497f0a
Grabee <grabee@fillygaming888.com>
2020-04-16 18:56:02 +0800

    Checkbox widget-all btn

--- lib/pages/checkbox.dart
+++ lib/pages/checkbox.dart
@@ -1,86 +1,6 @@
 import 'package:flutter/material.dart';
 
-//----------------ALL checkbox---------------------------------
-class CheckboxAllWidget extends StatefulWidget {
-	@override
-	_CheckboxAllWidgetState createState() => _CheckboxAllWidgetState();
-}
-
-class  _CheckboxAllWidgetState extends State<CheckboxAllWidget> {
-
-	Map<String, bool> titleName = {
-		'Stared': false,
-		'Half time': false,
-		'Full time': false,
-		'Goals': false,
-		'Red cards': false,
-		'Missed penalty': false,
-		'Lineup': false,
-	};
-
-	List<IconData> iconData = [
-		Icons.timer,
-		Icons.access_alarm,
-		Icons.alarm_add,
-		Icons.insert_emoticon,
-		Icons.class_,
-		Icons.donut_large,
-		Icons.blur_linear,
-	];
-
-	var tmpArray = [];
-
-	getCheckboxItems() {
-		titleName.forEach((key, value) {
-			if (value == true) {
-				tmpArray.add(key);
-			}
-		});
-		print(tmpArray);
-
-		tmpArray.clear();
-	}
-
-	cancelReturn(){   // 這邊要做返回上一個畫面
-		tmpArray.clear();
-		print(tmpArray);
-	}
-
-	@override
-	Widget build(BuildContext context) {
-		bool _allValue = false;
-		final checkAll = Checkbox(
-			value: _allValue,
-			onChanged: (bool newValue) {
-				setState(() {
-					_allValue = newValue;
-				});
-			},
-		);
-
-		return Column(
-			crossAxisAlignment: CrossAxisAlignment.center,
-			children: <Widget>[
-				Text("Set Alerts",
-						style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-				Checkbox(
-					value: _allValue,
-					activeColor: Colors.yellow,
-					checkColor: Colors.black,
-					onChanged: (bool value) {
-						setState(() {
-							_allValue = value;
-						});
-						print("我打勾：$value");
-					},
-				),
-			],
-		);
-	}
-}
-
 //----------------List checkbox---------------------------------
-
 class CheckboxWidget extends StatefulWidget {
   @override
   _CheckboxWidgetState createState() => _CheckboxWidgetState();
@@ -92,6 +12,8 @@ class _CheckboxWidgetState extends State<CheckboxWidget> {
 //	bool _value2 = false;
 	//we omitted the brackets '{}' and are using fat arrow '=>' instead, this is dart syntax
 //	void _value1Changed(bool value) => setState(() => _value1 = value);  // 在這裡處理畫面渲染或狀態變更
+
+	bool _allValue = false;
 
 	Map<String, bool> titleName = {
 		'Stared': false,
@@ -136,24 +58,22 @@ class _CheckboxWidgetState extends State<CheckboxWidget> {
 
 	@override
 	Widget build(BuildContext context) {
-		bool _allValue = false;
-		final checkAll=  CheckboxListTile(
-			title: Text("ALL", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-			value: false,
+		final checkAll = Checkbox(
+			tristate: false,
+			value: _allValue,
 			activeColor: Colors.yellow,
 			checkColor: Colors.black,
 			onChanged: (bool newValue) {
-//					titleName.update(
-//						key,(value) => value,
-//						ifAbsent: () => value,
-//					);
+				titleName.forEach((key, value) {
+					setState(() {
+						titleName[key] = newValue;
+					});
+				});
 				setState(() {
 					_allValue = newValue;
 				});
 			},
-			secondary: Text("Set Alerts"),
 		);
-
 		final keys = titleName.keys.toList();
 		final checkBoxes = List<Widget>.generate(keys.length, (i){
 			final key = keys[i];
@@ -164,6 +84,7 @@ class _CheckboxWidgetState extends State<CheckboxWidget> {
 				activeColor: Colors.yellow,
 				checkColor: Colors.black,
 				onChanged: (bool value) {
+//					判斷每一個子項目 結果是否相同, 全部為true or false -> 把tristate改成 null
 					setState(() {
 						titleName[key] = value;
 					});
@@ -174,7 +95,24 @@ class _CheckboxWidgetState extends State<CheckboxWidget> {
 
 		return Column(
 			children:
-//			checkAll
+			<Widget>[
+				Row(
+					mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//				往內縮間距
+					children: <Widget>[
+						Container(
+							child:Text("Set Alerts",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+						),
+						Row(
+								children: <Widget>[
+								Text("ALL",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+								checkAll
+							]
+						),
+					],
+				)
+			]
+				 +
 			checkBoxes + <Widget>[
 				ButtonBar(
 					children: <Widget>[
